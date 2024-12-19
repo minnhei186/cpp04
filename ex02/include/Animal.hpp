642d2a10ac6ee37f23fb3875172370ec0a3c713f
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2024-12-19 16:51:38 +0900

    mk02 ex02

--- ex02/include/Animal.hpp
+++ ex02/include/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: hosokawa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 12:16:00 by hosokawa          #+#    #+#             */
-/*   Updated: 2024/12/19 15:58:18 by hosokawa         ###   ########.fr       */
+/*   Updated: 2024/12/19 16:47:28 by hosokawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,6 @@ class Animal
 		Animal& operator=(const Animal& origin);
 		virtual ~Animal();
 
-		virtual void makeSound(void) const;
+		virtual void makeSound(void) const=0;
 		std::string     getType( void ) const;
 };
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hosokawa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 12:51:46 by hosokawa          #+#    #+#             */
-/*   Updated: 2024/12/19 15:32:41 by hosokawa         ###   ########.fr       */
+/*   Updated: 2024/12/19 16:50:35 by hosokawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@
 
 int main(void)
 {
+
+
+//	Animal a;        // 許可されない、コンパイルエラー
+//	Animal* a = new Animal(); // 許可されない、コンパイルエラー
 
 //	std::cout<<"-------test animal------"<<std::endl;
 //	Animal* animal=new Animal;	
@@ -74,27 +78,27 @@ int main(void)
 	}
 
 
-	std::cout<<"-------test wrong array animal------"<<std::endl;
-	Animal wrongArray_animal[4];
-	Cat wrongcat0;
-	Cat wrongcat1;
-	Dog wrongdog0;
-	Dog wrongdog1;
-
-	wrongArray_animal[0]=wrongcat0;
-	wrongArray_animal[1]=wrongcat1;
-	wrongArray_animal[2]=wrongdog0;
-	wrongArray_animal[3]=wrongdog1;
-
-	i=0;
-	while(i<4)
-	{
-		wrongArray_animal[i].makeSound();
-		i++;
-	}
-
-
-	std::cout<<"-------test finished------"<<std::endl;
+//	std::cout<<"-------test wrong array animal------"<<std::endl;
+//	Animal wrongArray_animal[4];
+//	Cat wrongcat0;
+//	Cat wrongcat1;
+//	Dog wrongdog0;
+//	Dog wrongdog1;
+//
+//	wrongArray_animal[0]=wrongcat0;
+//	wrongArray_animal[1]=wrongcat1;
+//	wrongArray_animal[2]=wrongdog0;
+//	wrongArray_animal[3]=wrongdog1;
+//
+//	i=0;
+//	while(i<4)
+//	{
+//		wrongArray_animal[i].makeSound();
+//		i++;
+//	}
+//
+//
+//	std::cout<<"-------test finished------"<<std::endl;
 
 
 	return 0;
